31dece812a43ac950ce50576e7a1d561119b4f5e
Altan <altanmk93@gmail.com>
2019-08-14 00:51:09 +0200

    changed log message

--- src/assignment2_publisher_subscriber/src/task2_2_2/subsriber.py
+++ src/assignment2_publisher_subscriber/src/task2_2_2/subsriber.py
@@ -3,7 +3,7 @@ from autominy_msgs.msg import SpeedCommand
 from std_msgs.msg import String
 
 def callback(raw_msg):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", raw_msg.value)
+    rospy.loginfo("Speed:  %s", raw_msg.value)
 
 def subscriber():
     rospy.init_node('subscriber_2', anonymous=True)
