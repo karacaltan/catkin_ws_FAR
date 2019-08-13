5e288029269b5e7f5c3eeedd19c431b36f051996
Fatih <doenmezfatih@outlook.com>
2019-08-13 18:33:27 +0200

    speed

---
+++ src/assignment2_publisher_subscriber/src/publisher.py
@@ -0,0 +1,35 @@
+# /usr/bin/env python
+
+import rospy
+from autominy_msgs.msg import Speed
+from std_msgs.msg import String
+import std_msgs.msg
+
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)  # 10hz
+
+    while not rospy.is_shutdown():
+        msg = "Message: %s" % rospy.get_time()
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+
+def pub_speed():
+    pub = rospy.Publisher('/sensors/speed', Speed, queue_size=10)
+    rospy.init_node('pub_speed')
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        header = std_msgs.msg.Header()
+        pub.publish(header, 12.0)
+        rate.sleep
+
+if __name__ == '__main__':
+    try:
+        #talker()
+        pub_speed()
+    except rospy.ROSInterruptException:
+        pass
