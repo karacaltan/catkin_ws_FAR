ec142bd25347b4ed877e565153aa1529de0efef0
Altan <altanmk93@gmail.com>
2019-08-20 12:21:18 +0200

    second version

--- src/assignment6/src/task2/lane_segmentation.py
+++ src/assignment6/src/task2/lane_segmentation.py
@@ -25,14 +25,14 @@ class LaneSegmentation:
         except CvBridgeError as e:
             print(e)
 
-        (rows, cols, channels) = cv_image.shape
+        rows, cols, channels = cv_image.shape
         if cols > 60 and rows > 60:
             cv2.circle(cv_image, (50, 50), 10, 255)
 
         cv2.imshow("Image window", cv_image)
 
-        image_to_binary(cv_image)
-        image_contours(image_to_binary(cv_image))
+        img = image_to_binary(cv_image)
+        crop_image(img)
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
@@ -42,23 +42,29 @@ class LaneSegmentation:
 
 def image_to_binary(image):
     th, dst = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
-    # cv2.imshow("Binary Image", dst)
     cv2.waitKey(3)
     return dst
 
 
-def image_contours(image):
-    im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def crop_image(image):
+    height, width = image.shape[:2]
+    start_now, start_col = int(height*.15), int(width*.15)
+    end_now, end_col = int(height * .85), int(width * .85)
+    cropped_img = image[start_now:end_now, start_col:end_col]
+    cv2.imshow("cropped", cropped_img)
+    im_gray = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(im_gray, 127, 255, 0)
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-    #print("Number of Contours found = " + str(len(contours)))
-    cv2.imshow('Contour Image', image)
+    filtered_contours = []
+    """for i in range(len(contours)):
+        if i < 34 or (i > 40 and i < 45):
+            filtered_contours.append(contours[i])"""
+    cv2.drawContours(cropped_img, contours, -1, (0, 255, 0), 3)
+    cv2.imshow('Contour Image', cropped_img)
 
 
 def main(args):
     ic = LaneSegmentation()
-    # rospy.init_node('image_converter', anonymous=True)
     try:
         rospy.spin()
     except KeyboardInterrupt:
