e0b76b8e3603f99ea7b05fc8507088d6271e5058
Altan <altanmk93@gmail.com>
2019-08-20 22:41:26 +0200

    distance between line and point

--- src/assignment6/src/lane_segmentation.py
+++ src/assignment6/src/lane_segmentation.py
@@ -8,6 +8,8 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import sys
+import imutils
+import numpy.linalg as alg
 
 
 class LaneSegmentation:
@@ -32,7 +34,10 @@ class LaneSegmentation:
         cv2.imshow("Image window", cv_image)
 
         img = image_to_binary(cv_image)
-        crop_image(img)
+        cropped = crop_image(img)
+        white = get_all_white_coordinates(cropped)
+        ransac(white)
+
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
@@ -55,14 +60,49 @@ def crop_image(image):
     im_gray = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(im_gray, 127, 255, 0)
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    filtered_contours = []
-    cv2.drawContours(cropped_img, contours, -1, (0, 255, 0), 3)
+    cv2.drawContours(cropped_img, contours, -1, (255, 255, 255), 3)
     cv2.imshow("cropped", cropped_img)
-    cv2.drawContours(image, filtered_contours, -1, (0, 255, 0), 3)
+    return cropped_img
+
+
+def get_all_white_coordinates(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    indices = np.where(gray == [255])
+    coordinates = zip(indices[0], indices[1])
+    return coordinates
+
+
+def ransac(coordinates):
+    random_sample_index = np.random.randint(0, len(coordinates) - 1)
+    random_sample_index2 = np.random.randint(0, len(coordinates) - 1)
+    fst_point = coordinates[random_sample_index]
+    snd_point = coordinates[random_sample_index2]
+    coordinates_without_samples = coordinates
+    del coordinates_without_samples[random_sample_index]
+    del coordinates_without_samples[random_sample_index2 - 1]
+    for coordinate in coordinates_without_samples:
+        distance = distance_point_model(np.asarray(fst_point), np.asarray(snd_point), np.asarray(coordinate))
+    slope, intercept = line_model(fst_point, snd_point)
+    return slope, intercept
+
+
+def line_model(fst_point, snd_point):
+    dy = snd_point[0] - fst_point[0]*1.0
+    dx = snd_point[1] - fst_point[1]
+    if dx == 0:
+        slope = 0
+    else:
+        slope = dy/dx
+    intercept = fst_point[0] - slope * fst_point[1]
+    return slope, intercept
+
+
+def distance_point_model(fst_point, snd_point, trd_point):
+    return alg.norm(np.cross(snd_point-fst_point, fst_point-trd_point))/alg.norm(snd_point-fst_point)
 
 
 def main(args):
-    ic = LaneSegmentation()
+    lane_segmentation = LaneSegmentation()
     try:
         rospy.spin()
     except KeyboardInterrupt:
@@ -72,3 +112,4 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv)
+
